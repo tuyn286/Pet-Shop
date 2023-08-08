a09e17930ed0f7a9f244e817b0803fd0592d1ebe
tuyen286 <nguyenhoangtuyen286@gmail.com>
2023-08-08 08:50:35 +0700

    tuyen commit

--- src/main/java/com/laptrinhweb/Entity/ProductEntity.java
+++ src/main/java/com/laptrinhweb/Entity/ProductEntity.java
@@ -3,5 +3,6 @@ package com.laptrinhweb.Entity;
 public class ProductEntity {
     private int productId;
     private String productName;
+    private String description;
     private Double price;
 }

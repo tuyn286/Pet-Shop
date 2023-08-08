01506607846a9aa47e1c09f1870266d3601aa537
tuyen286 <nguyenhoangtuyen286@gmail.com>
2023-08-08 09:27:13 +0700

    tuyn add bcrypt

---
+++ src/main/java/com/laptrinhweb/Hashing/HashingPassword.java
@@ -0,0 +1,21 @@
+package com.laptrinhweb.Hashing;
+
+import lombok.Data;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+public class HashingPassword {
+    public String hashedPass(String pass){
+        String salt = BCrypt.gensalt();
+        return BCrypt.hashpw(pass,salt);
+    }
+
+    public boolean isCorrectPass(String enteredPass,String hashed_pass){
+        if(BCrypt.checkpw(enteredPass,hashed_pass))
+            return true;
+        else
+            return false;
+    }
+}
